66e520f1f5c57228c475cd8e9c6d44e2055eb57d
Jack <jackclarke@jacks-mbp.home>
2018-12-02 10:56:15 +0000

    Init commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <div ref='test' id='test'></div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+var Marzipano = require('../../node_modules/marzipano')
+Vue.use(Marzipano)
+
+let element = null
+let viewer = null
+
+export default {
+  name: 'HelloWorld',
+  mounted: function () {
+    element = document.getElementById('test')
+    console.log(element)
+    viewer = new this.$marzipano.Viewer(element)
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
