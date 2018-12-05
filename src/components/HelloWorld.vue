cd73ddbe8bd8419e06858c056986ae0bfb15bb5b
Jack <jackclarke@jacks-mbp.home>
2018-12-05 11:11:16 +0000

    Get Marzipano basic example working in Vue & move tile assets to components directory

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <div ref='test' id='test'></div>
+    <div id='pano'></div>
   </div>
 </template>
 
@@ -11,34 +11,54 @@ Vue.use(Marzipano)
 
 export default {
   name: 'HelloWorld',
+
   mounted: function () {
-    const element = document.getElementById('test')
-    console.log(element)
-    const viewer = new Marzipano.Viewer(element)
+    var viewer = new Marzipano.Viewer(document.getElementById('pano'))
     console.log(viewer)
-  },
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+    var source = Marzipano.ImageUrlSource.fromString('//www.marzipano.net/media/cubemap/{f}.jpg')
+    var geometry = new Marzipano.CubeGeometry([{ tileSize: 1024, size: 1024 }])
+    var limiter = Marzipano.RectilinearView.limit.traditional(4096, 100 * Math.PI / 180)
+    var view = new Marzipano.RectilinearView(null, limiter)
+    var scene = viewer.createScene({
+      source: source,
+      geometry: geometry,
+      view: view,
+      pinFirstLevel: true
+    })
+    scene.switchTo()
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+* {
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  -webkit-user-drag: none;
+  -webkit-touch-callout: none;
+  -ms-content-zooming: none;
 }
-ul {
-  list-style-type: none;
+
+html, body {
+  width: 100%;
+  height: 100%;
   padding: 0;
+  margin: 0;
+  overflow: hidden;
+  font-family: helvetica, sans-serif;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+#pano {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 }
 </style>
